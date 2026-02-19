8435b3b2646d903c18fbd1902127c2383ece63f1
Elijah-Lewis <ejlewis919@gmail.com>
2026-02-19 17:26:35 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography, AppBar } from '@mui/material';
+import Button from '@mui/material/Button';
+
+export default function App() {
+  
+  return <> 
+  <AppBar></AppBar>
+  <Button variant="contained">Contained</Button> 
+  <Typography>TestText</Typography>
+  </>
+}
