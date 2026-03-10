01d2c2c16a06ef03a1a02becbb077cce6144dc67
Elijah-Lewis <55295746+Elijah-Lewis@users.noreply.github.com>
2026-03-09 23:23:20 -0400

    Adding changes for Passions and Photography Pages

--- src/Pages/Passions.tsx
+++ src/Pages/Passions.tsx
@@ -3,8 +3,29 @@ import { NavBar } from "../Components/NavBar";
 import { Typography, Box, Paper } from "@mui/material";
 
 // Asset Imports
-import lightning from "../assets/Screenshot 2025-04-21 194232.png";
-import ise from "../assets/Screenshot 2025-08-18 180652.png";
+//@ts-ignore
+import reading from "../assets/ReadingImage.JPEG";
+
+const NaturePhotos = import.meta.glob<{ default: string }>(
+  "../assets/PortfolioProjectNature/*.{png,jpg,jpeg,JPEG}",
+  { eager: true },
+);
+// Convert that object into a simple array of image paths
+const NaturePhotosPaths = Object.values(NaturePhotos).map((mod) => mod.default);
+
+const MusicPhotos = import.meta.glob<{ default: string }>(
+  "../assets/PortfolioProjectMusic/*.{png,jpg,jpeg,JPEG}",
+  { eager: true },
+);
+// Convert that object into a simple array of image paths
+const MusicPhotosPaths = Object.values(MusicPhotos).map((mod) => mod.default);
+
+const ArtPhotos = import.meta.glob<{ default: string }>(
+  "../assets/PortfolioProjectArt/*.{png,jpg,jpeg,JPEG}",
+  { eager: true },
+);
+// Convert that object into a simple array of image paths
+const ArtPhotosPaths = Object.values(ArtPhotos).map((mod) => mod.default);
 
 export function Passions() {
   // 1. Centralized Data: Simply add a new object here to create a new section
@@ -12,26 +33,26 @@ export function Passions() {
     {
       title: "Music",
       description: `From record collecting to Live shows, I love engaging with artists and their sonical creations.`,
-      image: lightning,
+      image: MusicPhotosPaths,
     },
     {
       title: "Art",
       description: `As I travel for internships and leisure, one thing that I ensure to do is to visit art museums.
         \nThe environments are often soothing and relaxing, encouraging visitors to enter an introspective state.`,
-      image: [lightning, ise],
+      image: ArtPhotosPaths,
     },
     {
       title: "Nature",
       description: `Throughout the world, there are few things that many people collectively agree are calming and exciting.
                     One of these things is nature, and I relate to the masses in my enjoyment of nature.
                     From beaches to mountains, nature never ceases to fascinate and soothe me.`,
-      image: lightning,
+      image: NaturePhotosPaths,
     },
     {
       title: "Books",
       description: `Whether it's a bedtime story or a fantasy getaway on a beach, books always come in clutch for quick enjoyment.
       Personally, historical fiction, and stories relating African American experiences are extremely exciting and intruiging.`,
-      image: ise,
+      image: reading,
     },
   ];
 
